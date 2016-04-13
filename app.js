4a0cbe4f5e0123cbf9564f0539daedca431478f4
Byronplchan <byronplchan@gmail.com>
2016-04-13 22:19:36 +0800

    Added files via upload
    this version of app.js :
    - takes input from front-end (index.html)
    - uses POST method to store user name and user comment into arrays ( nam[i], com[i] )
    - 'i' is incremented each time there is a POST

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var http = require ('http');
 var fs = require ('fs');
 var qs = require ('querystring');
+var i = 0;
+var nam = [];
+var com = [];
+
+
 
 var server = http.createServer(function (request, response) {
     if (request.method == 'GET') {
@@ -27,23 +32,29 @@ function onRequest (response) {
 
 //onSubmit takes POST data and writes it into 'forumContents.txt'
 function onSubmit(request, response) {
-        var body = '';
+
+    var body = '';
 
         request.on('data', function(data){
         body += data;
         });
 
         request.on('end', function(){
+        i++;
         var post= qs.parse(body);
-        fs.appendFile('forumContents.txt', post.name + " : " + post.comment + '\n', 'UTF-8', {'flags':'a+'});
-        console.log(post.name);
-        console.log(post.comment);
+        nam[i] = post.name;
+        com[i] = post.comment;
+
+        //fs.appendFile('forumContents.txt', post.name + " : " + post.comment + '\n', 'UTF-8', {'flags':'a+'});
+        console.log(nam[i]);
+        console.log(com[i]);
             response.writeHead(200, {"Content-Type": "text/html"});
             fs.createReadStream("./index.html").pipe(response);
         });
     }
 
 //function to read the updated file and send it back to index.html
+
 
 
 //Figure out how to make the front end constantly display the contents of 'forumContents.txt'
