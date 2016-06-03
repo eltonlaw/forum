52a2063e5321b73231d7370a223cebf9c57ac29e
EltonLaw <eltonlaw296@gmail.com>
2016-06-02 20:32:42 -0400

    Created backend express server and linked it with frontend

---
+++ public/javascript/app.module.js
@@ -0,0 +1,18 @@
+(function() {
+'use strict';
+
+angular.module('app', [
+	// ANGULAR
+	"ngRoute",
+	// CUSTOM
+	"app.core",
+	"app.forum",
+	"app.topic",
+	"app.discussion",
+	"app.profile",
+	"app.home",
+	"app.register",
+	"app.login",
+]);
+
+})();
---
+++ public/javascript/app.route.js
@@ -0,0 +1,63 @@
+(function() {
+'use strict';
+
+angular
+	.module('app')
+	.config(function($routeProvider) {
+	
+	$routeProvider
+		.when("/", {
+			templateUrl:"../views/home.view.html",
+			controller:"HomeCtrl",
+			controllerAs:'vm'
+		})
+		.when("/forum", {
+			templateUrl:"../views/forum.view.html",
+			controller:"ForumCtrl",
+			controllerAs:'vm'
+		})
+			.when("/forum/section1/topic1", {
+				templateUrl:"../views/topic.view.html",
+				controller:"TopicCtrl",
+				controllerAs:'vm'
+			})
+			.when("/forum/section1/topic1/1", {
+				templateUrl:"../views/discussion.view.html",
+				controller:"DiscussionCtrl",
+				controllerAs:'vm'
+			})
+
+		.when("/profile", {
+			templateUrl:"../views/profile.view.html",
+			controller:"ProfileCtrl",
+			controllerAs:'vm'
+		})
+		.when("/register", {
+			templateUrl:"../views/register.view.html",
+			controller:"RegisterCtrl",
+			controllerAs:'vm'
+		})
+			.when("/welcome", {
+				templateUrl:"../views/welcome.view.html",
+				controller:"RegisterCtrl",
+				controllerAs:'vm'
+			})
+		.when("/profile", {
+			templateUrl:"../views/profile.view.html",
+			controller:"ProfileCtrl",
+			controllerAs:'vm'
+		})
+		.when("/login", {
+			templateUrl:"../views/login.view.html",
+			controller:"ProfileCtrl",
+			controllerAs:'vm'
+		})
+		.when("/dashboard", {
+			templateUrl:"../views/dashboard.view.html",
+			controller:"DashboardCtrl",
+			controllerAs:'vm'
+		})
+		.otherwise({redirectTo:"/"})
+});
+
+})();
